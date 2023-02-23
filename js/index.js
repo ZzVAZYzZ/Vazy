5bf5ea2892d68d46e0004de6e41fa33a47c2e21f
Vazy <86739701+ZzVAZYzZ@users.noreply.github.com>
2023-02-23 18:49:57 +0700

    changed

--- js/index.js
+++ js/index.js
@@ -17,7 +17,7 @@ sun.onclick = function(){
     body.style.background = "#2D3436";
     body.style.color = "#CED6E0";
     colorMenu.style.background = "#CED6E0"
-    textShadoww[0].style.textShadow = "0 0 10px #dff9fb,0 0 20px #dff9fb,0 0 40px #dff9fb,0 0 80px #dff9fb,0 0 120px #dff9fb"
+    // textShadoww[0].style.textShadow = "0 0 10px #dff9fb,0 0 20px #dff9fb,0 0 40px #dff9fb,0 0 80px #dff9fb,0 0 120px #dff9fb"
     root.style.setProperty('--core_change2', rs.getPropertyValue('--core6'));
     root.style.setProperty('--core_change3', rs.getPropertyValue('--core5'));
     
@@ -25,11 +25,11 @@ sun.onclick = function(){
 moon.onclick = function(){
     sun.style.display = "block";
     moon.style.display = "none";
-    navbar[0].style.background = "#CED6E0";
-    body.style.background = "#DFE4EA"; 
+    navbar[0].style.background = "#DFE4EA";
+    body.style.background = "#CED6E0"; 
     body.style.color = "#2F3542"; 
     colorMenu.style.background = "#2F3542" ;
-    textShadoww[0].style.textShadow = "0 0 10px #2F3542,0 0 20px #2F3542,0 0 40px #2F3542,0 0 80px #2F3542,0 0 120px #2F3542";
+    // textShadoww[0].style.textShadow = "0 0 10px #2F3542,0 0 20px #2F3542,0 0 40px #2F3542,0 0 80px #2F3542,0 0 120px #2F3542";
     root.style.setProperty('--core_change2', rs.getPropertyValue('--core5'));
     root.style.setProperty('--core_change3', rs.getPropertyValue('--core6'));
 }
@@ -42,32 +42,39 @@ color.onclick = function(){
         i=0;
     }
 }
+var text = document.getElementsByClassName("second-text");
+
 var rs = getComputedStyle(root);
+text[0].style.color = rs.getPropertyValue('--core_change1');
+text[0].style.fontSize = 1.2 + 'em';
 core[0].onclick = function(){
     root.style.setProperty('--core_change1', rs.getPropertyValue('--core1'));
+    text[0].style.color = rs.getPropertyValue('--core1');
 }
 core[1].onclick = function(){
     root.style.setProperty('--core_change1', rs.getPropertyValue('--core2'));
+    text[0].style.color = rs.getPropertyValue('--core2');
 }
 core[2].onclick = function(){
     root.style.setProperty('--core_change1', rs.getPropertyValue('--core3'));
+    text[0].style.color = rs.getPropertyValue('--core3');
 }
 core[3].onclick = function(){
     root.style.setProperty('--core_change1', rs.getPropertyValue('--core4'));
+    text[0].style.color = rs.getPropertyValue('--core4');
 }
-var text = document.getElementsByClassName("second-text");
 function textLoad(){
     setTimeout(() => {
-        text[0].textContent = "Web Developer";
+        text[0].textContent = "Minh Hải";
     },0);
     setTimeout(() => {
-        text[0].textContent = "Web Designer";
+        text[0].textContent = "Web Developer";
     },3000);
     setTimeout(() => {
-        text[0].textContent = "Vazy-sama";
+        text[0].textContent = "Ux/Ui Designer";
     },6000);
     setTimeout(() => {
-        text[0].textContent = "Handsome";
+        text[0].textContent = "Hard-working";
     },9000);
 }
 textLoad();
@@ -75,6 +82,7 @@ setInterval(textLoad, 12000);
 
 var home = document.getElementById('home_button');
 var about = document.getElementById('about_button');
+var service = document.getElementById('service_button');
 var project = document.getElementById('project_button');
 var contact = document.getElementById('contact_button');
 var key = 0;
@@ -97,11 +105,16 @@ contact.onclick = function(){
     key=3;
     navKey();
 }
+service.onclick = function(){
+    key=4;
+    navKey();
+}
 function navKey(){
     home.style.color = null;
     about.style.color = null;
     project.style.color = null;
     contact.style.color = null;
+    service.style.color = null;
     if(key==0){
         home.style.color = rs.getPropertyValue('--core_key');
     }else if(key==1){
@@ -110,5 +123,8 @@ function navKey(){
         project.style.color = rs.getPropertyValue('--core_key');
     }else if(key==3){
         contact.style.color = rs.getPropertyValue('--core_key');
+    }
+    else if(key==4){
+        service.style.color = rs.getPropertyValue('--core_key');
     }
 }
